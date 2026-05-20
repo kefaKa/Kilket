928b1441caa39757bd7d4802f403f689cfa61c58
KefaKassahun <64911520+kefaKa@users.noreply.github.com>
2026-05-20 13:17:00 -0400

    Add error struct and helpers

--- src/include/error/error.h
+++ src/include/error/error.h
@@ -51,3 +51,51 @@ namespace kilket
         SYS_IO_FAILED,
         SYS_THREAD_FAILED,
         SYS_ALLOC_FAILED,
+
+        EMPTY_VALUE,
+        VALUE_NOT_FOUND,
+        INVALID_DEPTH,
+
+        UNKNOWN
+    };
+
+    struct StackFrame
+    {
+        std::string function;
+        std::string file;
+        uint32_t line;
+    };
+
+    struct FWError
+    {
+        ErrorCode code;
+        std::string message;
+
+        std::vector<StackFrame> stackTrace;
+
+        FWError(ErrorCode code, std::string message) : code(code), message(message) {}
+
+        void pushFrame(const std::source_location &loc)
+        {
+            stackTrace.push_back(StackFrame{
+                loc.function_name(),
+                loc.file_name(),
+                loc.line()
+            });
+        }
+
+        void printStackTrace() const
+        {
+            std::cerr << "  Trace (most recent last):\n";
+            for (const auto &f : stackTrace)
+                std::cerr << "    → " << f.function << " (" << f.file << ":" << f.line << ")\n";
+        }
+
+        static FWError make(ErrorCode code, std::string message, std::source_location loc = std::source_location::current())
+        {
+            FWError e(code, message);
+            e.pushFrame(loc);
+            return e;
+        }
+    };
+}
