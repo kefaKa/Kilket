dc02fead2993a3d7bd2fc6a3d5ecb8510bc35c6e
KefaKassahun <64911520+kefaKa@users.noreply.github.com>
2026-05-20 14:59:00 -0400

    Add remaining ConfigManager methods

--- src/include/config_manager.h
+++ src/include/config_manager.h
@@ -16,3 +16,15 @@ namespace kilket
         #endif
         private:
             nlohmann::json config_obj = nlohmann::json::object();
+
+            bool isflushed;
+            Result<nlohmann::json> convert_task_to_json(const Task &task);
+            Result<Task> convert_json_to_task(const nlohmann::json &json_task);
+
+            ConfigManager() = default;
+            Result<void> load(std::fstream &file);
+        public:
+            static Result<ConfigManager*> create();
+            ~ConfigManager();
+            nlohmann::json getjson() const { return config_obj; }
+            bool is_flushed() const { return isflushed; }
