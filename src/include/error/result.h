a291b7d01c2f56dd101470d3468740c2bc194231
KefaKassahun <64911520+kefaKa@users.noreply.github.com>
2026-05-20 14:24:00 -0400

    Add Result<T> error propagation helpers

--- src/include/error/result.h
+++ src/include/error/result.h
@@ -65,3 +65,67 @@ namespace kilket
         {
             return std::get<FWError>(data).code;
         }
+
+        std::string getErrMessage()
+        {
+            return std::get<FWError>(data).message;
+        }
+    };
+
+    // Void Specialization
+    template <>
+    class Result<void>
+    {
+    private:
+        std::optional<FWError> err_;
+
+        Result() = default;
+
+    public:
+        static Result<void> Ok()
+        {
+            return Result{};
+        }
+
+        static Result<void> Err(ErrorCode code, std::string message)
+        {
+            Result r;
+            r.err_ = FWError(code, std::move(message));
+            return r;
+        }
+
+        static Result<void> Err(FWError err)
+        {
+            Result r;
+            r.err_ = std::move(err);
+            return r;
+        }
+
+        bool isOk() const
+        {
+            return !err_.has_value();
+        }
+
+        bool isErr() const
+        {
+            return err_.has_value();
+        }
+
+        // Extraction
+        FWError unwrapErr()
+        {
+            return err_.value();
+        }
+
+        ErrorCode getErrCode()
+        {
+            return err_.value().code;
+        }
+
+        std::string getErrMessage()
+        {
+            return err_.value().message;
+        }
+
+    };
+}
