bfc6fa1aa17bd99d9bff1528f41fdbd38dbc4251
KefaKassahun <64911520+kefaKa@users.noreply.github.com>
2026-05-24 15:47:00 -0400

    Implement log line writing

--- src/session_logger.cpp
+++ src/session_logger.cpp
@@ -51,3 +51,69 @@ namespace kilket
         {
             return Result<void>::Ok(); // idempotent
         }
+
+        FW_LOG("[DEBUG] Starting a new log session ...");
+        file.open(file_path, ios::out | ios::app);
+        if (!file.is_open())
+        {
+            return Result<void>::Err(FWError::make(
+                ErrorCode::SYS_IO_FAILED, "Error: opening log file " + file_path + ". ✗"));
+        }
+        session["task_name"] = fs::path(file_path).filename().string();
+        session["working_directory"] = fs::path(file_path).parent_path().string();
+
+        auto now = std::chrono::system_clock::now();
+        std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+        std::stringstream ss;
+        ss << std::put_time(std::localtime(&now_time), "%Y-%m-%dT%H:%M:%S");
+
+        session["session-timestamp"] = ss.str();
+        session["session_log"] = json::array();
+        flushed = false;
+        is_running = true;
+
+        FW_LOG("[DEBUG] Log session started successfully.");
+        return Result<void>::Ok();
+    }
+
+    Result<void> SessionLogger::log_execution(const ExecutionResult &execution_result)
+    {
+        if(!is_running)
+        {
+            return Result<void>::Err(FWError::make(
+                ErrorCode::SESSION_LOGGER_NOT_RUNNING, "Error: session logger not initialized. ✗"));
+        }
+        if (session.empty())
+        {
+            return Result<void>::Err(FWError::make(
+                ErrorCode::SESSION_LOGGER_NOT_RUNNING, "Error: session logger not initialized. ✗"));
+        }
+        WatchEvent watch_event = execution_result._event;
+        string terminal_msg = execution_result.log;
+        vector<string> commands = execution_result.build_commands;
+        int success_code = execution_result.id;
+
+        json event;
+        event["event_type"] = "modify";
+        event["file_path"] = watch_event.path;
+        event["file_type"] = watch_event.filetype;
+        event["event_mask"] = watch_event.event_mask;
+
+        try
+        {
+            event["build-command"] = json::array();
+            for (auto &cmd : commands)
+            {
+                event["build-command"].push_back(cmd);
+            }
+            event["success_code"] = success_code;
+            event["terminal_msg"] = terminal_msg;
+
+            auto now = std::chrono::system_clock::now();
+            std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+            std::stringstream ss;
+
+            // thread-safe time
+            std::tm tm_buf;
+            localtime_r(&now_time, &tm_buf); // POSIX, thread-safe
+            ss << std::put_time(&tm_buf, "%Y-%m-%dT%H:%M:%S");
