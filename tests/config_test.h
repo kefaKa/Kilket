94184fcc9fe9ad4932d8b8db2431558e7e6f540c
KefaKassahun <64911520+kefaKa@users.noreply.github.com>
2026-05-24 11:12:00 -0400

    Add config test fixtures

---
+++ tests/config_test.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "../src/include/config_manager.h"
+#include "../src/include/json.hpp"
+
+namespace kilket
+{
+    // ---------------------------------------------------------------------------
+    // ConfigManagerTest
+    // ---------------------------------------------------------------------------
