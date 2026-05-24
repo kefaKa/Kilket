5f4603f7916a7747f747bf350ebb3b902609a1a2
KefaKassahun <64911520+kefaKa@users.noreply.github.com>
2026-05-24 14:25:00 -0400

    Add session ID generation to SessionLogger

--- src/include/session_logger.h
+++ src/include/session_logger.h
@@ -18,3 +18,17 @@ namespace kilket
             std::fstream file;
             std::string file_path;
             json session = json::object();
+
+            bool is_running = false;
+            bool flushed = false;
+            SessionLogger() = default;
+            Result<void> init(const std::string &path);
+        public:
+            static Result<SessionLogger*> create(const std::string &path);
+            ~SessionLogger();
+
+            Result<void> start();
+            Result<void> log_execution(const ExecutionResult &execution_result);
+            Result<void> stop();
+    };
+}
