7c666d6f2070fa52cebc8ab3b8f95a54b387bc82
KefaKassahun <64911520+kefaKa@users.noreply.github.com>
2026-06-18 12:24:00 -0400

    Add --task flag test case

--- cli/tests/test_init.sh
+++ cli/tests/test_init.sh
@@ -13,3 +13,21 @@ test_init_creates_task() {
   kilket init >/dev/null 2>&1
   local code=$?
   assert_exit 0 "$code" "init creates a task"
+
+  local files
+  files=$(ls "$KILKET_CONFIG_DIR_TEST" 2>/dev/null)
+  if [[ -z "$files" ]]; then
+    FAIL=$((FAIL+1))
+    echo "FAIL: init creates a task (no config file written)"
+  else
+    PASS=$((PASS+1))
+  fi
+}
+
+test_init_twice_fails() {
+  fresh_dir
+  kilket init >/dev/null 2>&1
+  local out
+  out=$(kilket init 2>&1)
+  assert_contains "$out" "Error:" "init twice should print an Error"
+}
